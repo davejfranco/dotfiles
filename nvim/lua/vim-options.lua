b06ab21002e5bc63f118fe998f09f1eceb60056a
Dave <davefranco1987@gmail.com>
2024-03-04 21:20:02 +0100

    nvim

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set number")
+-- Tab settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+-- Leader 
+vim.g.mapleader = " "
+
