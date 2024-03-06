1bedab4177b62b79b301d78a6e1618e767670442
Dave Franco <davefranco@Daves-MBP.office.onomondo.com>
2024-03-06 11:15:37 +0100

    moved to .config

--- nvim/lua/vim-options.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd("set number")
--- Tab settings
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
--- Leader 
-vim.g.mapleader = " "
-
