cbea29a32e6ceab63c6fff4ca7d205585d8a15ee
Dave Franco <davefranco1987@gmail.com>
2024-04-01 18:24:23 +0200

    back to catpuccin
    I cannot agree which theme to use

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,16 +1,20 @@
---[[return {
+return {
   "catppuccin/nvim",
   lazy = false,
-  name = "catppuccin",
+  name = "catppuccin-mocha",
   priority = 1000,
+  integrations = {
+    treesitter = true,
+    cmp = true,
+  },
   config = function()
     vim.cmd.colorscheme "catppuccin-mocha"
   end
-}--]]
-return {
+}
+--[[return {
 	"rebelot/kanagawa.nvim",
 	lazy = false,
 	config = function()
 		vim.cmd.colorscheme("kanagawa-wave")
 	end,
-}
+}--]]
