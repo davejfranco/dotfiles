2b1e883d6d036224df8fd46c6a4fdaa9f2417781
Dave Franco <davefranco1987@gmail.com>
2024-05-19 18:49:08 +0200

    move between buffers with tabs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,14 @@
 -- Lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,5 +17,3 @@ require("vim-options")
 -- Load lazyvim
 local opts = {}
 require("lazy").setup("plugins")
-
-
