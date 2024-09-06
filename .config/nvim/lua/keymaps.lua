758166dd9c6fa910dae23431a2f09496134f7d55
Dave Franco <davefranco1987@gmail.com>
2024-09-06 09:24:20 +0200

    keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,5 +2,10 @@
 vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true })
 -- Split Window
-vim.api.nvim_set_keymap("n", "<C-vs>", ":vsplit<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-hs>", ":split<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<c-vs>", ":vsplit<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<c-hs>", ":split<CR>", { noremap = true, silent = true })
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
