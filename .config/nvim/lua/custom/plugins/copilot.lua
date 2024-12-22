668b2c5eefd48391ebdbe2ee924f69388450590a
Dave Franco <davefranco1987@gmail.com>
2024-12-22 18:05:42 +0100

    Copilot with keymaps

---
+++ .config/nvim/lua/custom/plugins/copilot.lua
@@ -0,0 +1,31 @@
+return {
+  'github/copilot.vim',
+  event = 'BufEnter',
+  config = function()
+    -- Accept Copilot suggestion
+    vim.keymap.set('i', '<C-g>', function()
+      return vim.fn['copilot#Accept'] '\\<CR>'
+    end, { expr = true, silent = true, desc = 'Accept Copilot suggestion' })
+
+    -- Cycle suggestions (next)
+    vim.keymap.set('i', '<C-l>', '<Plug>(copilot-next)', { silent = true, desc = 'Cycle to next Copilot suggestion' })
+
+    -- Cycle suggestions (previous)
+    vim.keymap.set('i', '<C-h>', '<Plug>(copilot-previous)', { silent = true, desc = 'Cycle to previous Copilot suggestion' })
+
+    -- Dismiss suggestion
+    vim.keymap.set('i', '<C-x>', '<Plug>(copilot-dismiss)', { silent = true, desc = 'Dismiss Copilot suggestion' })
+
+    -- Enable Copilot
+    vim.keymap.set('n', '<C-e>', function()
+      vim.cmd 'Copilot enable'
+      print 'Copilot enabled'
+    end, { silent = true, desc = 'Enable Copilot' })
+
+    -- Disable Copilot
+    vim.keymap.set('n', '<C-d>', function()
+      vim.cmd 'Copilot disable'
+      print 'Copilot disabled'
+    end, { silent = true, desc = 'Disable Copilot' })
+  end,
+}
