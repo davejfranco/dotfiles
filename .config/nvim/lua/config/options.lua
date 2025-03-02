76c25c4839997303e24874cb392e9fc5dcaea4e4
Dave Franco <davefranco1987@gmail.com>
2025-03-02 11:23:35 +0100

    lazyvim first steps

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,36 @@
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Nerd Fonts
+vim.g.have_nerd_font = true
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Tab settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
