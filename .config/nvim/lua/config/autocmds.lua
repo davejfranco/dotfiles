830228e2b2e646cb519330e1c9ab98bf39033805
Dave Franco <davefranco1987@gmail.com>
2025-04-03 19:09:56 +0200

    more customizations
    - autocmd
    - blink
    - noice

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,25 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("BufWriteCmd", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.buftype == "" then
+      vim.cmd("write")
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+    pattern = ":w",
+    callback = function()
+        vim.cmd('silent! write')
+    end,
+})
+
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+    pattern = ":w!",
+    callback = function()
+        vim.cmd('silent! write!')
+    end,
+})
