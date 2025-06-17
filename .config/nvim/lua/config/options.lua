e5a059aa34221b6e16dabe569253407d6f85cd7e
Dave Franco <davefranco1987@gmail.com>
2025-06-17 18:22:40 +0200

    tf filetype

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -41,3 +41,12 @@ vim.cmd("set shiftwidth=2")
 vim.g.lazyvim_python_lsp = "pyright"
 -- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
+
+-- Filetype specific settings
+vim.filetype.add({
+  extension = {
+    tf = "terraform",
+    tofu = "terraform",
+    tfvars = "terraform",
+  }
+})
